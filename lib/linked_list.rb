09c03ee3cc4d3628760907982c5b5250b7d098ca
Andrew <andrewasmit@hotmail.com>
2023-03-30 14:35:44 -0500

    commit tree 1ac1ecb9b8c12163c7af068836ce5fbca5dfa725

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,25 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    if self.head.nil?
+      return nil
+    end
+    current_node = @head
+    target = @head
+    length = 1
+    while current_node.next_node
+      current_node = current_node.next_node
+      length += 1
+    end
+    if length - n < 0 || n < 1
+      return nil
+    else
+      target_idx = length - n 
+      target_idx.times do
+        target = target.next_node
+      end
+    target.value
+    end
   end
 
 end
